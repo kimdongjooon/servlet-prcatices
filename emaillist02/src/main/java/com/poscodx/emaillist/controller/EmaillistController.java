e19a2996e82c0d6be7f16367fc12a4573f2b32a6
kimdongjooon <ehdwns6745@naver.com>
2023-09-07 14:45:44 +0900

    emaillist02 - servlet/예제.

--- emaillist02/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
+++ emaillist02/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
@@ -3,6 +3,7 @@ package com.poscodx.emaillist.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,23 +18,41 @@ public class EmaillistController extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().print("<h1>ok<h1>");
+//		response.getWriter().print("<h1>ok<h1>");
+		request.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("a");
 		
 		if("add".equals(action)) {
 			
-		}else if("form".equals(action)) {
+			String firstName = request.getParameter("fn");
+			String lastName = request.getParameter("ln");
+			String email = request.getParameter("email");
 			
+			EmaillistVo vo = new EmaillistVo();
+			vo.setFirstName(firstName);
+			vo.setLastName(lastName);
+			vo.setEmail(email);
+			
+			new EmaillistDao().insert(vo);
+			
+			response.sendRedirect("/emaillist02/el");
+		}else if("form".equals(action)) {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/form.jsp");
+			rd.forward(request, response);
 		}else {
 			List<EmaillistVo> list = new EmaillistDao().findAll();
 			
+			request.setAttribute("list", list);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+			rd.forward(request, response);
 		}
 	}
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-//		doGet(request, response);
+		doGet(request, response);
 	}
 
 }
--- emaillist02/src/main/webapp/WEB-INF/views/add.jsp
+++ emaillist02/src/main/webapp/WEB-INF/views/add.jsp
@@ -6,17 +6,4 @@
 	pageEncoding="UTF-8"%>
 
 <%
-	request.setCharacterEncoding("utf-8");
-	String firstName = request.getParameter("fn");
-	String lastName = request.getParameter("ln");
-	String email = request.getParameter("email");
-	
-	EmaillistVo vo = new EmaillistVo();
-	vo.setFirstName(firstName);
-	vo.setLastName(lastName);
-	vo.setEmail(email);
-	
-	new EmaillistDao().insert(vo);
-	
-	response.sendRedirect("/emaillist01/index.jsp");
 %>
